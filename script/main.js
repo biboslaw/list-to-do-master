84f7a3166746e4453ef36bff27a899156cabda28
Sebastian Pieprzyk <pieprzyk.sebastian@gmail.com>
2018-03-19 17:25:48 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,150 +1,139 @@
-function createNewTask(task) {   
-    this.newTask = task   
+function createNewTask(task) {
+    this.newTask = task
 }
 
-function addNewEdit(newListEl, newElement){
+function addNewEdit(newListEl, newElement) {
     var newEdit = document.createElement('img')
     var apply = document.createElement('img')
     newEdit.setAttribute('src', 'images/edit.svg')
-    newEdit.classList.add('edit')
+    newEdit.setAttribute('id', 'edit')
     newListEl.appendChild(newEdit)
     apply.setAttribute('src', 'images/correct.svg')
-    apply.classList.add('apply')
+    apply.setAttribute('id', 'apply')
     apply.classList.add('hidden')
     newListEl.appendChild(apply)
     newElement.app
-    newEdit.addEventListener('click', function(e){
+    newEdit.addEventListener('click', function (e) {
         e.stopPropagation()
         editTask(e, newElement)
     })
-    apply.addEventListener('click', function(e){
+    apply.addEventListener('click', function (e) {
         e.stopPropagation()
         applyChanges(e, newElement)
         setStorage()
     })
-    
+
 }
 
-function addNewDelete(newListEl){
+function addNewDelete(newListEl) {
     var newDelete = document.createElement('img')
     newDelete.setAttribute('src', 'images/delete.svg')
     newListEl.appendChild(newDelete)
-    newDelete.addEventListener('click', function(e){
+    newDelete.addEventListener('click', function (e) {
         e.stopPropagation()
-        e.target.parentElement.setAttribute('class', 'toDelete')
-        var toDelete = document.querySelector('.toDelete')
+        e.target.parentElement.setAttribute('id', 'toDelete')
+        var toDelete = document.querySelector('#toDelete')
         e.target.parentElement.parentElement.removeChild(toDelete)
         isEmpty()
         setStorage()
     })
-    
+
 }
 
 function appendTask() {
     var ul = document.querySelector('ul')
-    if (ul.textContent == 'List is empty...'){
+    if (ul.textContent == 'List is empty...') {
         ul.textContent = ''
     }
-    var newTaskName = document.querySelector(".newTaskName").value
+    var newTaskName = document.querySelector(".addTask input").value
     createTask(newTaskName)
     setStorage()
 }
 
 function main() {
     // checking if localstorage is empty, if not, create new tasks of storage objects // 
-    if(localStorage.length>0){
+    if (localStorage.length > 0) {
         var temp
-        for (var i = 0; i<localStorage.length; i++){
-            temp = localStorage.getItem('name'+i)
+        for (var i = 0; i < localStorage.length; i++) {
+            temp = localStorage.getItem('name' + i)
             createTask(temp)
         }
     } else {
-    // if localStogare is empty, initialize tasks of example terms //
-    createTask('Write a letter.')
-    createTask('Clean up apartment.')
-    createTask('Take out garbage.')
-    createTask('Wash the dishes.')
+        // if localStogare is empty, initialize tasks of example terms //
+        createTask('Write a letter.')
+        createTask('Clean up apartment.')
+        createTask('Take out garbage.')
+        createTask('Wash the dishes.')
     }
-    
-    var clearStorage = document.querySelector('.clearButton')
+
+    var clearStorage = document.querySelector('#clearButton')
     clearStorage.addEventListener('click', clearStorageFunc)
     var addTask = document.querySelector("button")
-    var newTaskName = document.querySelector('.newTaskName')
+    var newTaskName = document.querySelector('.addTask input')
     newTaskName.focus()
     addTask.addEventListener("click", appendTask)
 }
 
-function createTask (newTaskName) {
+function createTask(newTaskName) {
     var newElement = new createNewTask(newTaskName)
     // creating input el. to editing task //
-   
-    //newInput.classList.add('hidden')
+
     var taskList = document.querySelector('.taskContent')
     var newLi = document.createElement('li')
     var p = document.createElement('p')
     p.textContent = newElement.newTask
     newLi.appendChild(p)
-    //newLi.appendChild(newInput)
     addNewDelete(newLi)
     addNewEdit(newLi, newElement)
     taskList.appendChild(newLi)
-    newLi.addEventListener('click', function(e){
+    newLi.addEventListener('click', function (e) {
         e.target.classList.toggle('correct')
     })
 }
 
-/*function editTask(e, newElement) {
-    e.target.previousSibling.previousSibling.classList.remove('hidden')
-    e.target.previousSibling.previousSibling.focus()
-    e.target.parentElement.firstChild.classList.add('hidden')
-    e.target.classList.toggle('hidden')
-    e.target.nextSibling.classList.toggle('hidden')
-}*/
-
-function editTask(e, newElement){
+function editTask(e, newElement) {
     var newInput = document.createElement('input')
     newInput.setAttribute('type', 'text')
-    newInput.setAttribute('placeholder', newElement.newTask);
-    newInput.classList.add('newInput')
+    newInput.setAttribute('placeholder', newElement.newTask)
     var thisLi = e.target.parentElement
     var thisP = thisLi.querySelector('p').classList.add('hidden')
     thisLi.insertBefore(newInput, thisP)
     newInput.focus()
-    thisLi.querySelector('.apply').classList.remove('hidden')
+    thisLi.querySelector('#apply').classList.remove('hidden')
     e.target.classList.add('hidden')
-    newInput.addEventListener('click', function(event){
+    newInput.addEventListener('click', function (event) {
         event.stopPropagation()
     })
 }
 
-function applyChanges (e, newElement) {
+function applyChanges(e, newElement) {
     var thisEl = e.target.parentElement
-    var thisElInput = thisEl.querySelector('.newInput')
+    var thisElInput = thisEl.querySelector('input')
     newElement = thisElInput.value
     thisEl.firstChild.textContent = newElement
     thisEl.removeChild(thisElInput)
     thisEl.firstChild.classList.remove('hidden')
     e.target.classList.toggle('hidden')
-    thisEl.querySelector('.edit').classList.remove('hidden')
+    thisEl.querySelector('#edit').classList.remove('hidden')
 }
 
-function isEmpty (){
+function isEmpty() {
     var taskCounter = document.querySelectorAll('li')
-    if (taskCounter.length === 0){
+    if (taskCounter.length === 0) {
         console.log(taskCounter.length)
         taskCounter = document.querySelector('ul')
         taskCounter.textContent = "List is empty..."
     }
 }
 
-function setStorage () {
+function setStorage() {
     var overwrite = document.querySelectorAll('p')
-    for (var j = 0; j<overwrite.length; j++){
+    for (var j = 0; j < overwrite.length; j++) {
         localStorage.setItem('name' + j, overwrite[j].textContent)
     }
 }
 
-function clearStorageFunc () {
+function clearStorageFunc() {
     localStorage.clear()
 }
 
